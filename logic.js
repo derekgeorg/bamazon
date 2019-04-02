1fba801f5bfc783b1ce00607a6dfffa27428b767
Derek <45375006+derekgeorg@users.noreply.github.com>
2019-04-02 16:30:04 -0500

    created and connected bamazon_db. connection tested successfully

--- logic.js
+++ logic.js
@@ -1,2 +1,16 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "P0SlavDu2",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+
+})
